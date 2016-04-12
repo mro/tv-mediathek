b8e1176b7e9dd9c90718b7f67f5fe95ebcc58f09
Marcus Rohrmoser <github@mro.name>
2016-04-12 17:07:33 +0200

    use ‚original‘ Atom SVG Logo from http://intertwingly.net/blog/2010/04/15/HTML-to-Atom

--- assets/atomenabled.svg.sh
+++ assets/atomenabled.svg.sh
@@ -6,8 +6,9 @@ cd "$(dirname "$0")"
 
 inkscape=/Applications/Inkscape.app/Contents/Resources/bin/inkscape
 
-$inkscape --help >/dev/null 2>&1 || { echo "Inkscape is not installed." && exit 1; }
-optipng -help >/dev/null 2>&1 || { echo "optipng is not installed." && exit 1; }
+$inkscape --help >/dev/null 2>&1  || { echo "Inkscape is not installed." && exit 1; }
+optipng -help >/dev/null 2>&1     || { echo "optipng is not installed." && exit 1; }
+sips --help >/dev/null 2>&1       || { echo "sips is not installed. https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/sips.1.html" && exit 1; }
 
 src="$(pwd)/atomenabled-dev.svg"
 dst_dir="$(pwd)"
@@ -22,6 +23,9 @@ $inkscape --export-width=210 --export-height=80 --export-png=$dst $OPTS --file="
 dst=${prefix}-100x100.png
 $inkscape --export-width=100 --export-height=100 --export-area=0:0:81:81 --export-png=$dst $OPTS --file="$src"
 
+sips --setProperty description 'AtomEnabled.org Logo' \
+  --setProperty copyright https://github.com/mro/tv-mediathek/blob/master/assets/atomenabled.svg \
+  "$dst_dir"/*.png
 optipng -o 7 "$dst_dir"/*.png
 
 # crate a clean, stripped down SVG
@@ -30,12 +34,14 @@ dst=${prefix}.svg
 cp "$src" "$dst"
 # http://stackoverflow.com/a/10492912
 $inkscape "$dst" \
+  --select=graphics0 --verb=EditDelete \
+  --select=graphics0 --verb=EditDelete \
   --select=graphics1 --verb=EditDelete \
   --select=graphics2 --verb=EditDelete \
-  --select=helper0 --verb=EditDelete \
-  --select=helper1 --verb=EditDelete \
-  --select=text0 --verb=EditDelete \
-  --select=bitmap0 --verb=EditDelete \
-  --verb=FileVacuum --verb=FileSave \
-  --verb=FileClose --verb=FileQuit
+  --select=helper0   --verb=EditDelete \
+  --select=helper1   --verb=EditDelete \
+  --select=text1     --verb=EditDelete \
+  --select=bitmap0   --verb=EditDelete \
+  --verb=FileVacuum  --verb=FileSave \
+  --verb=FileClose   --verb=FileQuit
 $inkscape --without-gui --vacuum-defs --export-plain-svg="$dst" --file="$dst"
